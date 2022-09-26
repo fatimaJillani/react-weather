4c301ce3aa838fa7a96241abd1458785bebe71e6
fatimaJillani <106700215+fatimaJillani@users.noreply.github.com>
2022-09-26 13:38:57 -0400

    worked on WeatherInfo CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
--- src/Weather.js
+++ src/Weather.js
@@ -1,75 +1,51 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { ColorRing } from "react-loader-spinner";
+import React from "react";
+import "./Weather.css";
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function Weather() {
-  const [city, setCity] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
-
-  function displayWeather(response) {
-    setLoaded(true);
-    setWeather({
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description,
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = "094780c710fa4efd669f0df8c3991927";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Enter a city..."
-        onChange={updateCity}
-      />
-
-      <input type="submit" value="Search" />
-    </form>
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control search-input"
+            />
+          </div>
+          <div className="col-3 p-0">
+            <input
+              type="submit"
+              value="Search"
+              className="btn
+        btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <div className="WeatherInfo">
+        <div className="row">
+          <div className="col-6">
+            <h1>New York</h1>
+            <ul>
+              <li>Saturday 16:53, clear sky Humidity: 40%, Wind: 3.13km/h</li>
+            </ul>
+          </div>
+          <div className="col-6">
+            <div className="temp-container d-flex justify-content-end">
+              <ReactAnimatedWeather
+                icon="CLEAR_DAY"
+                color="goldenrod"
+                size={52}
+                animate={true}
+              />
+              <span className="temp">21</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
-
-  if (loaded) {
-    return (
-      <div>
-        {form}
-        <ul>
-          <li>Temperature: {Math.round(weather.temperature)}°C</li>
-          <li>Description: {weather.description}</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {weather.wind}km/h</li>
-          <li>
-            <img src={weather.icon} alt={weather.description} />
-          </li>
-        </ul>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        {form}
-        <ColorRing
-          visible={true}
-          height="180"
-          width="180"
-          ariaLabel="blocks-loading"
-          wrapperStyle={{}}
-          wrapperClass="blocks-wrapper"
-          colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
-        />
-      </div>
-    );
-  }
 }
